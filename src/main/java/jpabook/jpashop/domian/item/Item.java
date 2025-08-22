03d4bfa93c5d5330b006cb7be77121544f127fda
ssm <tjtkdan1072@gmail.com>
2025-08-22 09:03:26 +0900

    상품 엔티티 개발(비즈니스 로직 추가)

--- src/main/java/jpabook/jpashop/domian/item/Item.java
+++ src/main/java/jpabook/jpashop/domian/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domian.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domian.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,25 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    // == 비지니스 로직 == //
+    // -> stockQuantity을 가지고있는 Item에서 로직을 넣어주는게 응집도 측면에서 좋은 설계다.
+    // -> 그리하여 위에 @Setter를 선언하는거 보다는 밑에처럼 필요한 비지니스 로직을 만들어두는게 좋다.
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
