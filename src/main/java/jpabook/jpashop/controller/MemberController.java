2279a6020e24edc0b13ca959ee55ccfe24e48d4a
ssm <tjtkdan1072@gmail.com>
2025-08-23 12:29:36 +0900

    회원 등록 적용

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.controller;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domian.Address;
+import jpabook.jpashop.domian.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String submit(@Valid MemberForm form, BindingResult result) {
+        // 화면에서 form이 넘어왔을때 @Valid을 보고 유효성이 걸리면 result에 에러가 들어가고 다시 화면으로 돌려준다.
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
