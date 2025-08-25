416c99f0313c576de8340680b51473f22d4b5f9a
ssm <tjtkdan1072@gmail.com>
2025-08-25 11:19:17 +0900

    상품 수정 페이지 적용

--- src/main/java/jpabook/jpashop/domian/item/Book.java
+++ src/main/java/jpabook/jpashop/domian/item/Book.java
@@ -2,13 +2,25 @@ package jpabook.jpashop.domian.item;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jpabook.jpashop.controller.BookForm;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @DiscriminatorValue("B")
 @Getter @Setter
-public class Book extends Item{
+public class Book extends Item {
     private String author;
     private String isbn;
+
+    public static Book createBook(BookForm bookForm) {
+        Book book = new Book();
+        book.setName(bookForm.getName());
+        book.setPrice(bookForm.getPrice());
+        book.setStockQuantity(bookForm.getStockQuantity());
+        book.setAuthor(bookForm.getAuthor());
+        book.setIsbn(bookForm.getIsbn());
+
+        return book;
+    }
 }
